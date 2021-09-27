bb22d892c1c2c8739597de1a64c577c0c45e4216
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-09-27 15:17:56 +0200

    Create action to load questions from api

--- src/redux/actions/actionCreators.ts
+++ src/redux/actions/actionCreators.ts
@@ -1,1 +1,21 @@
-export {};
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import actionTypes from './actionTypes';
+
+const url = 'https://opentdb.com/api.php?amount=50&category=9&type=multiple';
+
+export default function loadQuestions() {
+  return async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios(url);
+      dispatch({
+        type: actionTypes.LOAD_QUESTIONS,
+        questions: data
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.LOAD_QUESTIONS_ERROR
+      });
+    }
+  };
+}
