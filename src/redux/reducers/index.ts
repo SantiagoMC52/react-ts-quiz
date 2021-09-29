f569b5441b258a9730a62622d65a513ff52f9125
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-09-29 12:44:13 +0200

    Remove comments

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import questionsReducer from './questionsReducer';
-// import questionReducer from './questionReducer';
 
 export const rootReducer = combineReducers({
   questionsReducer
-//   questionReducer
 });
 
 export default rootReducer;
