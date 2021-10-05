70f1f3ffe3e3994f906e1806daf542b836938179
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-05 18:51:39 +0200

    Destrututing questions with anwers on reducer

--- src/redux/reducers/questionsReducer.ts
+++ src/redux/reducers/questionsReducer.ts
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
@@ -16,7 +15,13 @@ type Action = {
 
 function questionsReducer(allQuestions:IQuestions = initialState, action: Action) {
   if (action.type === actionTypes.LOAD_QUESTIONS) {
-    return action.questions;
+    const { questions } = action;
+    const newQuestions = questions.map((question: any) => ({
+      ...question,
+      answers: [{ item: question.correct_answer, answer: true }, ...question.incorrect_answers
+        .map((item: any) => ({ item, answer: false }))].sort(() => Math.random() - 0.5)
+    }));
+    return newQuestions;
   }
   return allQuestions;
 }
